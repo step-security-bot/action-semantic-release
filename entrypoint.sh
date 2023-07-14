b94b0b4c08986544d58d333c7ca98563409c0d61
Jay Mirecki <43052836+jaymirecki@users.noreply.github.com>
2023-07-14 15:27:13 -0500

    Revert "fix(branches): make 'main' a string" (#12)
    This reverts commit fee047c88ef9276b50a64b2cd04e8ea3a93cce77.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/sh -l
 
-github_token=$1
-branches=$2
-dry_run=$3
-
 git config --global --add safe.directory "${GITHUB_WORKSPACE:=.}"
 
-export GITHUB_TOKEN=$github_token
+export GITHUB_TOKEN=$1
 
-npx semantic-release --branches $branches --dryRun $dry_run
+npx semantic-release
