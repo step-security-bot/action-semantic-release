af7e06dbbd6f25bf455eea427c1904e15080704a
Jay Mirecki <43052836+jaymirecki@users.noreply.github.com>
2023-07-14 15:09:59 -0500

    feat(branches): Support prerelease branches. Also add CI tests (#9)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh -l
 
+github_token=$1
+branches=$2
+dry_run=$3
+
 git config --global --add safe.directory "${GITHUB_WORKSPACE:=.}"
 
-export GITHUB_TOKEN=$1
+export GITHUB_TOKEN=$github_token
 
-npx semantic-release
+npx semantic-release --branches $branches --dryRun $dry_run
